7dca462c955d257dc8d31185aa9d59d95ba8a962
Afroman726 <dymitrihawkins@gmail.com>
2022-05-10 06:50:23 -0700

    Made my first button using JavaScriptgit status

---
+++ Practice1/practice.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm") .innerHTML = "Transaction confirmed! Check Etherscan to review stats on transaction";
+    document.getElementById("button").style.display="none";
+}
