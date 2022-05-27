6e673f0328d8c653331ccae0945119fccf232df6
Afroman726 <dymitrihawkins@gmail.com>
2022-05-26 19:18:40 -0700

    Made a simple JavaScript function that slices the last letter of a string

---
+++ Practice_3/practice.js
@@ -0,0 +1,8 @@
+//This function will take the last letter of the string
+function lastLetter(text){
+    let str = text;
+    const result = str.slice(-1);
+    console.log(result);
+}
+
+lastLetter("yeet");
